8ad8b6711e05bf124948cffbc686259870c5e396
adir1 <aa7381@bs.amalnet.k12.il>
2024-04-06 15:49:01 +0300

    commit 2

--- app/src/main/java/com/example/mission13/MainActivity.java
+++ app/src/main/java/com/example/mission13/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.mission13;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    double a , b , c ;
-        Random rnd = new Random();
-        EditText eT , eT2 , eT3;
-        Button btn , btn2 ;
+    double a, b, c;
+    Random rnd = new Random();
+    EditText eT, eT2, eT3;
+    Button btn, btn2;
+
+    boolean A = false;
+    boolean B = false;
+    boolean C = false;
+    int count =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +30,68 @@ public class MainActivity extends AppCompatActivity {
         eT = findViewById(R.id.eT);
         eT2 = findViewById(R.id.eT2);
         eT3 = findViewById(R.id.eT3);
-
-
     }
-    public void clicked(View view)
+
+
+
+
+    public void enter_a(View view)
     {
-        a = rnd.nextInt(100)+1;
-        b = rnd.nextInt(100)+1;
-        c = rnd.nextInt(100)+1;
+        if(eT.getText().toString().isEmpty()) {
+            String textValue1 = eT.getText().toString();
+            a = Double.parseDouble(textValue1);
+            count ++;
+        }else if(!eT.getText().toString().isEmpty())
+    {
+        count++;
+    }
     }
 
+    public void enter_b(View view)
+    {
+        if(eT.getText().toString().isEmpty()) {
+            String textValue1 = eT.getText().toString();
+            b = Double.parseDouble(textValue1);
+            count ++;
+        }else if(!eT.getText().toString().isEmpty())
+        {
+            count++;
+        }
+    }
+    public void enter_c(View view)
+    {
+        if(eT.getText().toString().isEmpty()) {
+            String textValue1 = eT.getText().toString();
+            c = Double.parseDouble(textValue1);
+            count ++;
+        }else if(!eT.getText().toString().isEmpty())
+        {
+            count++;
+        }
+    }
+    public void clicked(View view) {
+        a = rnd.nextInt(100) + 1;
+        eT.setText(String.valueOf(a));
+
+        b = rnd.nextInt(100) + 1;
+        eT2.setText(String.valueOf(b));
+
+        c = rnd.nextInt(100) + 1;
+        eT3.setText(String.valueOf(c));
+
+    }
+
+    public void go(View view) {
+
+
+
+
+
+            Intent si = new Intent(this, abcActivity2.class);
+            si.putExtra("result", a);
+            si.putExtra("result2", b);
+            si.putExtra("result3", c);
+            startActivity(si);
+
+    }
 }
--- app/src/main/java/com/example/mission13/abcActivity2.java
+++ app/src/main/java/com/example/mission13/abcActivity2.java
@@ -1,6 +1,9 @@
 package com.example.mission13;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class abcActivity2 extends AppCompatActivity {
+    double a , b , c ;
+    TextView tV , tV2 , tV3 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_abc2);
+        tV = findViewById(R.id.tV);
+        tV2 = findViewById(R.id.tV2);
+        tV3 = findViewById(R.id .tV3);
+
+        Intent gi = getIntent();
+        a = gi.getDoubleExtra("result", -1);
+        b = gi.getDoubleExtra("result2", -1);
+        c = gi.getDoubleExtra("result3", -1);
+
+        tV.setText(String.valueOf(a));
+        tV2.setText(String.valueOf(b));
+        tV3.setText(String.valueOf(c));
 
     }
-}
+
+        public void go_back(View view) {
+
+        finish();
+    }
+
+    }
+
